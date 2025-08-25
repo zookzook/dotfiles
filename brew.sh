bde792cfd9b0ce4130ec945c2701e0e885d08c3d
zookzook <zookzook@unitybox.de>
2025-08-25 11:49:02 +0200

    Initial commit

---
+++ brew.sh
@@ -0,0 +1,107 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Save Homebrew’s installed location.
+BREW_PREFIX=$(brew --prefix)
+
+# Install GNU core utilities (those that come with macOS are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
+
+# Install some other useful utilities like `sponge`.
+brew install moreutils
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+brew install findutils
+# Install GNU `sed`, overwriting the built-in `sed`.
+brew install gnu-sed --with-default-names
+
+# Install `wget` with IRI support.
+brew install wget --with-iri
+
+# Install GnuPG to enable PGP-signing commits.
+brew install gnupg
+
+# Install more recent versions of some macOS tools.
+brew install vim --with-override-system-vi
+brew install grep
+brew install openssh
+brew install screen
+
+# Install some CTF tools; see https://github.com/ctfs/write-ups.
+brew install aircrack-ng
+brew install bfg
+brew install binutils
+brew install binwalk
+brew install cifer
+brew install dex2jar
+brew install dns2tcp
+brew install fcrackzip
+brew install foremost
+brew install hashpump
+brew install hydra
+brew install john
+brew install knock
+brew install netpbm
+brew install nmap
+brew install pngcheck
+brew install socat
+brew install sqlmap
+brew install tcpflow
+brew install tcpreplay
+brew install tcptrace
+brew install ucspi-tcp # `tcpserver` etc.
+brew install xpdf
+brew install xz
+
+# Install other useful binaries.
+brew install ack
+brew install git
+brew install git-lfs
+brew install gs
+brew install imagemagick --with-webp
+brew install lua
+brew install lynx
+brew install p7zip
+brew install pigz
+brew install pv
+brew install rename
+brew install rlwrap
+brew install ssh-copy-id
+brew install tree
+brew install vbindiff
+brew install zopfli
+brew install k9s
+brew install bat
+brew install fd
+brew install procs
+brew install dust
+brew install ripgrep
+brew install tokei
+brew install bandwhich
+brew install jq
+brew install mongodb-community@8.0
+brew install eza
+brew install wxwidget
+brew install libvips
+brew install sqlite3
+brew install ansible
+brew install zig
+brew install --cask ghostty
+
+# Remove outdated versions from the cellar.
+brew cleanup
+
+# asdf
+brew install asdf
+asdf install elixir 1.18.3-otp-27
+asdf install erlang 28.0.2 --with-ssl=$(brew --prefix openssl)
+asdf global elixir 1.18.3-otp-27
+asdf global elixir erlang 28.0.2
